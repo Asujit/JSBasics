2fee62562800be2be347a4dcdd1bff5441eaec9c
Asujit <autisujit24@gmail.com>
2025-11-12 11:04:18 +0530

    CountVowels

---
+++ CountVowels.js
@@ -0,0 +1,14 @@
+function count(str){
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+
+    for(let i=0; i<str.length; i++){
+        if(vowels.includes(str[i])){
+            count ++;
+        
+        }
+    }
+    return count;
+}
+
+console.log(count("Hello Sujit"));
