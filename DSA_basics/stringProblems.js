56a9c18b8e584a51665db78cd2ea09337df029bd
Asujit <autisujit24@gmail.com>
2026-02-23 23:58:30 +0530

    findVowelsandCount

--- DSA_basics/stringProblems.js
+++ DSA_basics/stringProblems.js
@@ -13,3 +13,19 @@ function reverseString(str){
 }
 
 console.log(reverseString("Hey , What are you doing?"));
+
+
+function findVowelandCount(str){
+    let vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    let result = [];
+
+    for(let i=1; i<str.length; i++){
+        if(vowels.includes(str[i])){
+            result.push(str[i]);
+        }
+    }
+    console.log(result.length);
+    return result;
+}
+
+console.log(findVowelandCount("Hey what are you doing?"));
